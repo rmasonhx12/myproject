52adb18fa43504878c79049afbeae98dc6312d1a
Robert Hendricks <rmasonh22@gmail.com>
2024-09-22 12:40:58 -0400

    update vue to dynamically inject message into html

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
-
+  export default {
+    data() {
+      return {
+        message: 'Welcome to Bujisoft Developemt with Vue3'
+      };
+    }
+  };
 </script>
 
 <template>
-  <h1>Hello World from Bujisoft LLC</h1>
+  <h1>{{ message }}</h1>
 </template>
 
 <style>
